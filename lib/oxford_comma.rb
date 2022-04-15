b273f2957ae150b71e6731db12eb7c7a4a98e010
Sean <craig.sean92@gmail.com>
2022-04-14 22:54:00 -0600

    commit tree f6730758f951663ba77de1bb2d5eebb048029de9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    case array.length 
+    when 1 
+        array.join
+    when 2
+        array.join(' and ')
+    else 
+       array[-1] = "and " + array[-1]
+       return array.join(", ")
+    end
+    
 end
